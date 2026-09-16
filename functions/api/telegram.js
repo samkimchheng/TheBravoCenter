6259d9119d308a864e0aff2e998260a5be13b4a9
samkimchheng <camboview41@gmail.com>
2026-09-16 17:47:18 +0700

    fix: update correct telegram group chat ID

--- functions/api/telegram.js
+++ functions/api/telegram.js
@@ -17,7 +17,7 @@ export async function onRequestPost({ request, env }) {
     }
 
     const botToken = '8516708366:AAEA1b9lWQC2fMpzeZU5ePnm2Y6t5RV4LS8';
-    const chatId = '-1005436333674';
+    const chatId = '-1004478158551';
 
     if (!botToken || !chatId) {
       return new Response(JSON.stringify({ error: 'Telegram Token or Chat ID is missing.' }), {
